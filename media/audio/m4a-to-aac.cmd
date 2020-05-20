cd641d569a107fa1870ff038d9526f936e8b4d44
pfilaretovEPAM <pfilaretov@idbs.com>
2020-05-20 10:59:30 +0300

    background with multiple audio combined

---
+++ media/audio/m4a-to-aac.cmd
@@ -0,0 +1,2 @@
+rem "C:/Program Files/ffmpeg-4.2.2-win64-static/bin/ffmpeg" -i D1.m4a -acodec copy D1.aac
+"C:/Program Files/ffmpeg-4.2.2-win64-static/bin/ffmpeg" -i E1.m4a -acodec libmp3lame -ab 256k E1.mp3
--- src/com/pfilaretov/test/xuggler/MergeImageWithAudioExample.java
+++ src/com/pfilaretov/test/xuggler/MergeImageWithAudioExample.java
@@ -23,40 +23,70 @@ import javax.imageio.ImageIO;
 public class MergeImageWithAudioExample {
 
     private static final String OUTPUT_FILENAME = "C:/Users/Petr_Filaretov/IdeaProjects/XugglerTest/media/MergeImageWithAudioExample.mp4";
-    private static final String C1_AUDIO_PATH = "C:/Users/Petr_Filaretov/IdeaProjects/XugglerTest/media/audio/C1.mp3";
     private static final String BACKGROUND_PATH = "C:/Users/Petr_Filaretov/IdeaProjects/XugglerTest/media/img/video-background-2.jpg";
+    private static final String C1_AUDIO_PATH = "C:/Users/Petr_Filaretov/IdeaProjects/XugglerTest/media/audio/C1.mp3";
+    private static final String D1_AUDIO_PATH = "C:/Users/Petr_Filaretov/IdeaProjects/XugglerTest/media/audio/D1.mp3";
+    private static final String E1_AUDIO_PATH = "C:/Users/Petr_Filaretov/IdeaProjects/XugglerTest/media/audio/E1.mp3";
 
     public static void main(String[] args) {
-        IContainer containerAudio = IContainer.make();
+        // C1
+        IContainer c1AudioContainer = IContainer.make();
 
         // check files are readable
-        if (containerAudio.open(C1_AUDIO_PATH, IContainer.Type.READ, null) < 0) {
-            throw new IllegalArgumentException("Cant find " + C1_AUDIO_PATH);
+        if (c1AudioContainer.open(C1_AUDIO_PATH, IContainer.Type.READ, null) < 0) {
+            throw new IllegalArgumentException("Cannot find " + C1_AUDIO_PATH);
         }
         // read audio file and create stream
-        int numAudioStreams = containerAudio.getNumStreams();
-        IStreamCoder coderAudio = null;
-        for (int i = 0; i < numAudioStreams; i++) {
-            IStream stream = containerAudio.getStream(i);
+        IStreamCoder c1AudioCoder = null;
+        for (int i = 0; i < c1AudioContainer.getNumStreams(); i++) {
+            IStream stream = c1AudioContainer.getStream(i);
             IStreamCoder coder = stream.getStreamCoder();
 
             if (coder.getCodecType() == ICodec.Type.CODEC_TYPE_AUDIO) {
-                coderAudio = coder;
+                c1AudioCoder = coder;
             }
         }
 
-        if (coderAudio == null) {
-            throw new IllegalArgumentException("Cannot find audio stream");
+        if (c1AudioCoder == null) {
+            throw new IllegalArgumentException("Cannot find audio stream for " + C1_AUDIO_PATH);
         }
 
-        if (coderAudio.open(null, null) < 0) {
-            throw new IllegalArgumentException("Cant open audio coder");
+        if (c1AudioCoder.open(null, null) < 0) {
+            throw new IllegalArgumentException("Cannot open audio coder for " + C1_AUDIO_PATH);
         }
+
+        // D1
+        IContainer d1AudioContainer = IContainer.make();
+
+        // check files are readable
+        if (d1AudioContainer.open(D1_AUDIO_PATH, IContainer.Type.READ, null) < 0) {
+            throw new IllegalArgumentException("Cannot find " + D1_AUDIO_PATH);
+        }
+        // read audio file and create stream
+        IStreamCoder d1AudioCoder = null;
+        for (int i = 0; i < d1AudioContainer.getNumStreams(); i++) {
+            IStream stream = d1AudioContainer.getStream(i);
+            IStreamCoder coder = stream.getStreamCoder();
+
+            if (coder.getCodecType() == ICodec.Type.CODEC_TYPE_AUDIO) {
+                d1AudioCoder = coder;
+            }
+        }
+
+        if (d1AudioCoder == null) {
+            throw new IllegalArgumentException("Cannot find audio stream for " + D1_AUDIO_PATH);
+        }
+
+        if (d1AudioCoder.open(null, null) < 0) {
+            throw new IllegalArgumentException("Cannot open audio coder for " + D1_AUDIO_PATH);
+        }
+
+
+
+
 
         // make a IMediaWriter to write the file.
         final IMediaWriter writer = ToolFactory.makeWriter(OUTPUT_FILENAME);
-
-        Dimension screenBounds = Toolkit.getDefaultToolkit().getScreenSize();
 
         // take the background image and convert to the right image type
         BufferedImage backgroundImage = getBackgroundImage();
@@ -66,32 +96,47 @@ public class MergeImageWithAudioExample {
         // at position 0, and that it will have a fixed frame rate of FRAME_RATE.
         int videoInputIndex = 0;
         int videoStreamId = 0;
-        int videoStreamIndex = writer.addVideoStream(videoInputIndex, videoStreamId, ID.CODEC_ID_MPEG4,
-            screenBounds.width / 2, screenBounds.height / 2);
+        // these are min values that works for background image, magic numbers...
+        int width = 750;
+        int height = 468;
+        int videoStreamIndex = writer.addVideoStream(videoInputIndex, videoStreamId, ID.CODEC_ID_MPEG4, width, height);
 
         int audioInputIndex = 1;
         // TODO - audioStreamId is the same as videoStreamId???
         int audioStreamId = 0;
-        int channelCount = 1; //coderAudio.getChannels()
-        int sampleRate = coderAudio.getSampleRate();
+        int channelCount = 1; //c1AudioCoder.getChannels()
+        int sampleRate = c1AudioCoder.getSampleRate();
         int audioStreamIndex = writer.addAudioStream(audioInputIndex, audioStreamId, channelCount, sampleRate);
 
 
         IPacket packet = IPacket.make();
         long startTime = System.nanoTime();
 
-        while (containerAudio.readNextPacket(packet) >= 0) {
+        while (c1AudioContainer.readNextPacket(packet) >= 0) {
             writer.encodeVideo(videoStreamIndex, backgroundImage, System.nanoTime() - startTime, TimeUnit.NANOSECONDS);
 
-            IAudioSamples samples = IAudioSamples.make(512, coderAudio.getChannels(), IAudioSamples.Format.FMT_S32);
-            coderAudio.decodeAudio(samples, packet, 0);
+            IAudioSamples samples = IAudioSamples.make(512, c1AudioCoder.getChannels(), IAudioSamples.Format.FMT_S32);
+            c1AudioCoder.decodeAudio(samples, packet, 0);
             if (samples.isComplete()) {
                 writer.encodeAudio(audioStreamIndex, samples);
             }
         }
 
-        coderAudio.close();
-        containerAudio.close();
+        while (d1AudioContainer.readNextPacket(packet) >= 0) {
+            writer.encodeVideo(videoStreamIndex, backgroundImage, System.nanoTime() - startTime, TimeUnit.NANOSECONDS);
+
+            IAudioSamples samples = IAudioSamples.make(512, d1AudioCoder.getChannels(), IAudioSamples.Format.FMT_S32);
+            d1AudioCoder.decodeAudio(samples, packet, 0);
+            if (samples.isComplete()) {
+                writer.encodeAudio(audioStreamIndex, samples);
+            }
+        }
+
+        c1AudioCoder.close();
+        c1AudioContainer.close();
+
+        d1AudioCoder.close();
+        d1AudioContainer.close();
 
         // tell the writer to close and write the trailer if needed
         writer.close();
